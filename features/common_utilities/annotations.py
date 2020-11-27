b621cdeb9a9c69a197c15228b4c651925ec603ab
samaythakkar <samay.gfs@gmail.com>
2020-11-27 20:04:47 +0000

    changed chome size

--- features/common_utilities/annotations.py
+++ features/common_utilities/annotations.py
@@ -29,6 +29,7 @@ class Annotations:
                 print("Browser Path:", driver_path)
                 chrome_options = Options()
                 chrome_options.add_argument('--headless')
+                chrome_options.add_argument("--window-size=1920x1080")
                 chrome_options.add_argument('--no-sandbox')
                 chrome_options.add_argument('--disable-dev-shm-usage')
                 pytest.driver = webdriver.Chrome(driver_path,chrome_options=chrome_options)
